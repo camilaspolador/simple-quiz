a480ff99fe2a745c7e1e645d6d5541caf7364cb5
Camila Matos Spolador <cmspola@gmail.com>
2023-08-01 20:31:35 -0300

    Initial Commit

---
+++ AlternativeRockNano2/AlternativeRockNano2App.swift
@@ -0,0 +1,17 @@
+//
+//  AlternativeRockNano2App.swift
+//  AlternativeRockNano2
+//
+//  Created by Camila Spolador on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct AlternativeRockNano2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlternativeRockNano2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AlternativeRockNano2
+//
+//  Created by Camila Spolador on 01/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
